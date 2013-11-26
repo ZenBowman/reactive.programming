b34dcc1b08d30967fca4eb64c7b404bfac0f1866
prasan.samtani <prasan.samtani@hulu.com>
2013-11-26 14:27:39 -0800

    Implemented nextRequest

--- assignment3/nodescala/src/main/scala/nodescala/nodescala.scala
+++ assignment3/nodescala/src/main/scala/nodescala/nodescala.scala
@@ -111,7 +111,16 @@ object NodeScala {
      *  @param relativePath    the relative path on which we want to listen to requests
      *  @return                the promise holding the pair of a request and an exchange object
      */
-    def nextRequest(): Future[(Request, Exchange)] = ???
+    def nextRequest(): Future[(Request, Exchange)] = {
+      val p = Promise[(Request, Exchange)]
+      createContext {
+        result => {
+          p.success((result.request, result))
+          removeContext()
+        }
+      }
+      p.future
+    }
   }
 
   object Listener {
